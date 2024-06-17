0896edebdc818a1a573f23c1e545586b9da4a9ed
KiTaeUk <ktw1421@naver.com>
2024-06-17 17:45:11 +0900

    [START] 2024-NC2-M20-ScreenTime

---
+++ 2024-NC2-M20-ScreenTime/2024-NC2-M20-ScreenTime/_024_NC2_M20_ScreenTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _024_NC2_M20_ScreenTimeApp.swift
+//  2024-NC2-M20-ScreenTime
+//
+//  Created by TEO on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct _024_NC2_M20_ScreenTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
