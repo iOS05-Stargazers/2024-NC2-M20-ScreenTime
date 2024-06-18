8e35143a506772bebb96d8073ced7bd6021cd820
YunJiLee <lyj9161@naver.com>
2024-06-18 11:01:09 +0900

    [Feat] Color Set, Image Set 추가
    black과 white의 경우, swift자체 컬러랑 네이밍이 겹쳐서 각각 blackt, whitet로 명명해두었어요!

---
+++ 2024-NC2-M20-ScreenTime/2024-NC2-M20-ScreenTime/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  2024-NC2-M20-ScreenTime
+//
+//  Created by Lee YunJi on 6/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        ZStack{
+           Rectangle()
+                .frame(maxHeight: 243)
+                .foregroundColor(.main)
+        }
+    }
+}
+
+#Preview{
+    MainView()
+}
