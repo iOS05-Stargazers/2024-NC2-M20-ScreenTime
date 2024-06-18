f015eeb35ccb80abb8dffbd7f4a675de16f56d12
KiTaeUk <ktw1421@naver.com>
2024-06-18 14:21:22 +0900

    [Feat] 뷰 구현 90% #5

---
+++ 2024-NC2-M20-ScreenTime/2024-NC2-M20-ScreenTime/Shape+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  Shape+Extension.swift
+//  2024-NC2-M20-ScreenTime
+//
+//  Created by TEO on 6/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+extension View {
+  func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+    clipShape(RoundedCorner(radius: radius, corners: corners))
+  }
+}
+
+struct RoundedCorner: Shape {
+  
+  var radius: CGFloat = .infinity
+  var corners: UIRectCorner = .allCorners
+  
+  func path(in rect: CGRect) -> Path {
+    let path = UIBezierPath(
+      roundedRect: rect,
+      byRoundingCorners: corners,
+      cornerRadii: CGSize(width: radius, height: radius)
+    )
+    return Path(path.cgPath)
+  }
+}
