f19e4aff1d3e754f72ed150568d7cc1d95705d0a
KiTaeUk <ktw1421@naver.com>
2024-06-19 00:32:10 +0900

    [Feat] 세부사항 수정 #5

--- 2024-NC2-M20-ScreenTime/2024-NC2-M20-ScreenTime/RoutinePrepareView.swift
+++ 2024-NC2-M20-ScreenTime/2024-NC2-M20-ScreenTime/RoutinePrepareView.swift
@@ -13,7 +13,6 @@ struct RoutinePrepareView: View {
     @EnvironmentObject var managedSettingModel: ManagedSettingModel
     @State var isPresented = false
     
-    let dummyIcons = ["image", "messages", "maps","phone","faceTime"]
     
     var body: some View {
         NavigationStack{
@@ -35,8 +34,13 @@ struct RoutinePrepareView: View {
                         
                     }
                 
-                AllowingApp()
-                    .padding(.bottom, 20)
+                VStack(alignment:.center) {
+                    AllowingApp()
+                    SelectApp()
+                }
+                .padding(.bottom, 20)
+
+                
                 AllowingTime()
                 
                 Spacer()
@@ -75,115 +79,113 @@ struct RoutinePrepareView: View {
 extension RoutinePrepareView {
     @ViewBuilder
     private func AllowingApp() -> some View {
-        VStack(alignment:.center) {
-            HStack{
-                VStack(alignment:.leading){
-                    Text("제한 앱 설정")
-                        .font(.callout)
-                        .bold()
-                    Text("사용할 수 있는 앱을 선택하여 방해를 줄일 수 있어요")
-                        .font(.caption)
-                }
-                .padding(.leading, 26)
-                Spacer()
+        HStack{
+            VStack(alignment:.leading){
+                Text("제한 앱 설정")
+                    .font(.callout)
+                    .bold()
+                Text("사용할 수 있는 앱을 선택하여 방해를 줄일 수 있어요")
+                    .font(.caption)
             }
+            .padding(.leading, 26)
+            Spacer()
+        }
+        
+    }
+    
+    
+    @ViewBuilder
+    private func SelectApp() -> some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.white)
+                .frame(width: 334, height: 102)
+                .shadow(radius: 10)
+                .opacity(0.3)
             
-            ZStack{
-                RoundedRectangle(cornerRadius: 12)
-                    .fill(.white)
-                    .frame(width: 334, height: 102)
-                    .shadow(radius: 10)
+            HStack(spacing: 0){
+                Button{
+                    isPresented = true
+                } label: {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(width: 167, height: 102)
+                        .cornerRadius(12, corners: [.topLeft, .bottomLeft])
+                        .overlay{
+                            VStack{
+                                HStack{
+                                    Text("일시 허용")
+                                        .foregroundColor(.main)
+                                        .font(.caption)
+                                        .padding(.leading)
+                                        .padding(.top)
+                                    Spacer()
+                                }
+                                Spacer()
+                                
+                                if managedSettingModel.isSelectionEmpty() {
+                                    Image("emptygroup")
+                                        .frame(height: 32)
+                                        .padding(.bottom, 22)
+                                    
+                                } else {
+                                    HStack{
+                                        ForEach(Array(managedSettingModel.selectionToDiscourage.applicationTokens), id: \.self) { token in
+                                            Label(token)
+                                                .labelStyle(IconOnlyLabelStyle())
+                                                .frame(width: 32, height: 32)
+                                                .cornerRadius(8)
+                                                .padding(-10)
+                                        }
+                                        ForEach(Array(managedSettingModel.selectionToDiscourage.categoryTokens), id: \.self) { token in
+                                            Label(token)
+                                                .labelStyle(IconOnlyLabelStyle())
+                                                .frame(width: 32, height: 32)
+                                                .cornerRadius(8)
+                                                .padding(-10)
+                                        }
+                                    }
+                                    .padding(.bottom, 26)
+                                }
+                            }
+                        }
+                }
+                
+                Divider()
+                    .frame(height: 102) // Divider의 높이를 사각형과 동일하게 설정
+                    .background(.gray)
                     .opacity(0.3)
                 
-                
-                HStack(spacing: 0){
-                    
-                    Button{
-                        isPresented = true
-                    } label: {
-                        Rectangle()
-                            .fill(.white)
-                            .frame(width: 167, height: 102)
-                            .cornerRadius(12, corners: [.topLeft, .bottomLeft])
-                            .overlay{
-                                VStack{
-                                    HStack{
-                                        Text("일시 제한")
-                                            .foregroundColor(.main)
-                                            .font(.caption)
-                                            .padding(.leading)
-                                        Spacer()
-                                    }
-                                    .padding(.bottom, 24)
-                                    
-                                    if managedSettingModel.isSelectionEmpty() {
-                                        HStack{
-                                            ForEach(dummyIcons, id: \.self) { icon in
-                                                Image("\(icon)")
-                                                    .resizable()
-                                                    .frame(width: 32, height: 32)
-                                                    .cornerRadius(8)
-                                                    .padding(-10)
-
-                                            }
-                                        }
-                                        .padding(.bottom, 10)
-                                    } else {
-                                        HStack{
-                                            ForEach(Array(managedSettingModel.selectionToDiscourage.applicationTokens), id: \.self) { token in
-                                                Label(token)
-                                                    .labelStyle(IconOnlyLabelStyle())
-                                                    .frame(width: 32, height: 32)
-                                                    .cornerRadius(8)
-                                                    .padding(-10)
-                                            }
-                                            ForEach(Array(managedSettingModel.selectionToDiscourage.categoryTokens), id: \.self) { token in
-                                                Label(token)
-                                            }
-
-                                        }
-                                    }
-                                    
-                                }
-                            }
+                Button{
+                    if let appSettings = URL(string: "App-prefs:SCREEN_TIME&path=ALWAYS_ALLOWED"), UIApplication.shared.canOpenURL(appSettings) {
+                        UIApplication.shared.open(appSettings)
                     }
+                } label: {
+                    Rectangle()
+                        .fill(.white)
+                        .frame(width: 167, height: 102)
+                        .cornerRadius(12, corners: [.topRight, .bottomRight])
+                        .overlay{
+                            VStack{
+                                HStack{
+                                    Text("항상 허용")
+                                        .foregroundColor(.main)
+                                        .font(.caption)
+                                        .padding(.leading)
+                                        .padding(.top)
 
-                    
-                    
-                    Divider()
-                        .frame(height: 102) // Divider의 높이를 사각형과 동일하게 설정
-                        .background(.gray)
-                        .opacity(0.3)
-                    
-                    Button{
-                        if let appSettings = URL(string: "App-prefs:SCREEN_TIME&path=ALWAYS_ALLOWED"), UIApplication.shared.canOpenURL(appSettings) {
-                               UIApplication.shared.open(appSettings)
-                       }
-                    } label: {
-                        Rectangle()
-                            .fill(.white)
-                            .frame(width: 167, height: 102)
-                            .cornerRadius(12, corners: [.topRight, .bottomRight])
-                            .overlay{
-                                VStack{
-                                    HStack{
-                                        Text("항상 제한")
-                                            .foregroundColor(.main)
-                                            .font(.caption)
-                                            .padding(.leading)
-                                        Spacer()
-                                    }
-                                    
+                                    Spacer()
                                 }
+                                Spacer()
+                                Image("icongroup")
+                                    .frame(height: 24)
+                                    .padding(.bottom, 26)
                             }
-                    }
+                        }
                 }
             }
         }
     }
-    
-    
-    
     
     
     
@@ -218,9 +220,12 @@ extension RoutinePrepareView {
                         }
                     }
             }
-
         }
     }
+    
+    
+    
+    
 }
 
 
